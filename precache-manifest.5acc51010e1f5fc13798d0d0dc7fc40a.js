3516609856ec62c10c7e5b4c0c0ff3237c04f519
Jesper <jesper.stoltz@gmail.com>
2019-04-27 19:21:47 +0200

    Updates

---
+++ precache-manifest.5acc51010e1f5fc13798d0d0dc7fc40a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e5dfd5d33820344a5e284fe5bc66afe",
+    "url": "/mandatory-advanced-js4/static/media/PermanentMarker.6e5dfd5d.ttf"
+  },
+  {
+    "revision": "b3ca65f14fe385e2fcb20caea59dd027",
+    "url": "/mandatory-advanced-js4/static/media/App_Background.b3ca65f1.jpg"
+  },
+  {
+    "revision": "eb147c3d7ae77fe3eefb",
+    "url": "/mandatory-advanced-js4/static/js/runtime~main.78de8069.js"
+  },
+  {
+    "revision": "d4846bb084337139b125",
+    "url": "/mandatory-advanced-js4/static/js/main.58c5bdf7.chunk.js"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/mandatory-advanced-js4/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "d4846bb084337139b125",
+    "url": "/mandatory-advanced-js4/static/css/main.7aa741bb.chunk.css"
+  },
+  {
+    "revision": "329834f09807ad23bf65423c81330871",
+    "url": "/mandatory-advanced-js4/index.html"
+  }
+];
